90965b0d12632111c798965cffbd097eff52ee21
Md Milon <abireshan32@gmail.com>
2025-02-01 23:43:37 +0600

    add feasurd section

--- src/myComponents/Featured/Featured.tsx
+++ src/myComponents/Featured/Featured.tsx
@@ -1,12 +1,40 @@
 import { useGetAllProductsQuery } from '@/Redux/Features/ProductMangement/getAllProdcuts';
 import React from 'react';
-
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Button } from '@/components/ui/button';
+  
 const Featured = () => {
     const { data: product } = useGetAllProductsQuery(undefined);
     console.log(product);
     return (
         <div className='mt-10'>
            <h2 className="text-4xl font-bold text-center mb-6">Featured Products</h2>
+           <div>
+                <div className="grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-2">
+                     {product?.data?.map((product) => (
+                         <Card key={product.id}>
+                         <CardHeader>
+                          <img className='' src={product.image} alt="" />
+                         </CardHeader>
+                         <CardContent>
+                         <CardTitle>${product.price}</CardTitle>
+                         <CardTitle>{product.title}</CardTitle>
+                         </CardContent>
+                         <CardFooter className='flex justify-between'>
+                            <Button className="bg-black text-white">Add to Cart</Button>
+                            <Button className="bg-black text-white">Buy Now</Button>
+                         </CardFooter>
+                       </Card>
+                     ))}
+                </div>
+           </div>
+           <Button className="mt-10 bg-black text-white flex justify-self-center">View All Products</Button>
         </div>
     );
 };
